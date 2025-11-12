ff60e8deb665295c64ae71fac2fdb9339330f9ed
Daniel O'Brien <20101948@setu.ie>
2025-11-12 15:53:59 +0000

    removed commented out code

--- lambdas/getMovieById.ts
+++ lambdas/getMovieById.ts
@@ -26,32 +26,6 @@ export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
         body: JSON.stringify({ message: "Missing query parameters" }),
  };
  }
-//     if (!isValidQueryParams(movieId)) {
-//       return {
-//         statusCode: 500,
-//         headers: {
-//           "content-type": "application/json",
-//  },
-//         body: JSON.stringify({
-//           message: `Incorrect type. Must match Query parameters schema`,
-//           schema: schema.definitions["MovieCastMemberQueryParams"],
-//  }),
-//  };
-//  }
-//     const movieId = parseInt(queryParams.movieId);
-//         let commandInput: QueryCommandInput = {
-//           TableName: process.env.TABLE_NAME,
-//      };
-  //       if ("cast" in queryParams) {
-  //         commandInput = {
-  //    ...commandInput,
-  //           KeyConditionExpression: "movieId = :m and begins_with(actorName, :a)",
-  //           ExpressionAttributeValues: {
-  //             ":c": movieId,
-  //             ":r": queryParams.roleName,
-  //    },
-  //   };
-  // }
 
     const commandOutput = await ddbDocClient.send(
       new GetCommand({
